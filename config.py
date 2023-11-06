039bcfac9f35e111637f4eb2e06df61e4e178a77
Firepup Sixfifty <firepyp650@gmail.com>
2023-11-05 18:51:42 -0600

    Config to it's own file

---
+++ config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+__version__ = "v1.0.5"
+npbase = "\[\x0303last\.fm\x03\] [A-Za-z0-9_[\]{}\\|^]{1,MAX} (is listening|last listened) to: \x02.+ - .*\x02 \([0-9]+ plays\)( \[.*\])?"
+servers = {
+    "ircnow": {
+        "address": "localhost",
+        "port": 6601,
+        "interval": 200,
+        "pass": env["ircnow_pass"],
+        "channels": {"#random": 0, "#dice": 0, "#offtopic": 0},
+        "admins": ["firepup", "h|thelounge", "firepup|lounge"],
+    },
+    "efnet": {
+        "address": "irc.mzima.net",
+        "channels": {"#random": 0, "#dice": 0},
+        "admins": ["firepup", "h|tl"],
+    },
+    "replirc": {
+        "address": "localhost",
+        "pass": env["replirc_pass"],
+        "channels": {"#random": 0, "#dice": 0, "#main": 0, "#bots": 0, "#firebot": 0},
+        "admins": ["firepup", "firepup|lounge", "h|tl"],
+    },
+}
