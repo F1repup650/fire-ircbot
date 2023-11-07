61c5b8f791636e7a9a345337ec2c784765bd70af
Firepup Sixfifty <firepyp650@gmail.com>
2023-11-07 06:51:55 -0600

    Need the env here

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from os import environ as env
+from dotenv import load_dotenv
+load_dotenv()
 __version__ = "v1.0.5"
 npbase = "\[\x0303last\.fm\x03\] [A-Za-z0-9_[\]{}\\|^]{1,MAX} (is listening|last listened) to: \x02.+ - .*\x02( \([0-9]+ plays\)( \[.*\])?)?"
+su = "^(su|sudo|(su .*|sudo .*))$"
 servers = {
     "ircnow": {
         "address": "localhost",
