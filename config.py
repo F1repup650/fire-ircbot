d03d54640b5615cf1930951a77b8d50f604a52e9
Firepup Sixfifty <firepyp650@gmail.com>
2023-11-07 09:20:41 -0600

    Bump version

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ as env
 from dotenv import load_dotenv
 import re
 load_dotenv()
-__version__ = "v1.0.5"
+__version__ = "v2.0.2"
 npbase = "\[\x0303last\.fm\x03\] [A-Za-z0-9_[\]{}\\|^]{1,MAX} (is listening|last listened) to: \x02.+ - .*\x02( \([0-9]+ plays\)( \[.*\])?)?"
 su = "^(su|sudo|(su .*|sudo .*))$"
 servers = {
